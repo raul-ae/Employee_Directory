18502e3e2fddbf93b8846a1a18888fb4e1c1742d
raul-ae <raul.ae@hotmail.com>
2020-08-18 17:56:54 -0500

    adding the componenets to render employees

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,29 +1,42 @@
-import React from 'react';
-import EmployeeCard from "./components/FriendCard";
+import React, { Component } from 'react';
+import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import employees from "./employees.json";
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.employees to the employees json array
+  state = {
+    employees
+  };
+
+  removeEmployee = id => {
+    // Filter this.state.employees for employees with an id not equal to the id being removed
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    // Set this.state.employees equal to the new employees array
+    this.setState({ employees });
+  };
+
+  // Map over this.state.employees and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employees List</Title>
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            removeEmployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            department={employee.department}
+            email={employee.email}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
